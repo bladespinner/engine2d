34a5c882fc5b6f1f987fda15c7e49fff6859d27b
Mihail Gospodinov <bladespinner@gmail.com>
2017-12-03 18:51:53 +0200

    Implement base drawable

--- src/engine/drawable.js
+++ src/engine/drawable.js
@@ -1,9 +1,36 @@
+'use strict';
+
+import { Point } from '../math/point';
+import { Frame } from './frame';
+
 /**
  * Drawable class
  */
 class Drawable {
-    draw() {
-        console.log('dick');
+    /**
+     * Construct a new drawable.
+     * @param {Point} position The location at which the drawable should.
+     * @param {function(Frame,Frame):void} render Drawable rendering function.
+     */
+    constructor(position, render) {
+        this.position = position;
+        this.render = render;
+    }
+    /**
+     * Draw the drawable
+     * @param {Frame} prevFrame previous animation frame.
+     * @param {Frame} curFrame current animation frame.
+     * @returns {void}
+     */
+    draw(prevFrame, curFrame) {
+        var ctx = curFrame.context;
+
+        ctx.save();
+        ctx.translate(this.position.x, this.position.y);
+        this.render(prevFrame, curFrame);
+        ctx.restore();
+
+        throw 'draw() Not Implemented for this Drawable.';
     }
 }
 
--- src/engine/frame.js
+++ src/engine/frame.js
@@ -3,6 +3,11 @@
  * and a reference to the context
  */
 class Frame {
+    /**
+     * Construct a new frame
+     * @param {CanvasRenderingContext2D} context Canvas rendering
+     * context reference
+     */
     constructor(context) {
         this.timestamp = Date.now();
         this.context = context;
