3c4092dd4b6568995511a5c6f5f8324e337fc448
Mihail Gospodinov <bladespinner@gmail.com>
2017-12-04 04:00:16 +0200

    Add drawable end promise

--- src/engine/drawable.js
+++ src/engine/drawable.js
@@ -10,11 +10,21 @@ class Drawable {
     /**
      * Construct a new drawable.
      * @param {Point} position The location at which the drawable should.
-     * @param {function(Frame,Frame):void} render Drawable rendering function.
+     * @param {function(Frame, Frame, Drawable):void} render Drawable rendering
+     * function.
      */
     constructor(position, render) {
         this.position = position;
         this.render = render;
+
+        /**
+         * A promise resolved when the drawable no longer needs to be rendered.
+         * @type {Promise<void>}
+         */
+        this.end = new Promise((resolve, reject) => {
+            this.finish = resolve;
+            this.interrupt = reject;
+        });
     }
     /**
      * Draw the drawable
@@ -27,7 +37,7 @@ class Drawable {
 
         ctx.save();
         ctx.translate(this.position.x, this.position.y);
-        this.render(prevFrame, curFrame);
+        this.render(prevFrame, curFrame, this);
         ctx.restore();
 
         throw 'draw() Not Implemented for this Drawable.';
