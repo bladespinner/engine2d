b498f0224b443df059bb0e67c0d9bc7e5be24d8f
Mihail Gospodinov <bladespinner@gmail.com>
2017-12-04 11:05:59 +0200

    Render drawables on the canvas

--- src/game/game.js
+++ src/game/game.js
@@ -1,8 +1,30 @@
 import { AnimationFrameScheduler } from '../engine/scheduler';
 import { Engine } from '../engine/engine';
+import { Drawable } from '../engine/drawable';
+import { Point } from '../math/point';
+import * as shapes from '../drawingPrimitives/shapes';
+
+import { FpsMeter } from './drawables/fpsMeter';
+import { Clear } from './drawables/clear';
 
 import './style/style.scss';
 import rarePepe from './assets/Rare-Pepe-Illuminati.png';
+
+/**
+ * Circle rendering drawable
+ * @returns {Drawable} drawable which renders a static circle
+ */
+function cirleDrawable() {
+    var position = new Point(150, 50);
+
+    return new Drawable(position, (prevFrame, curFrame) => {
+        var ctx = curFrame.context;
+        ctx.strokeStyle = '#44aa44';
+        ctx.lineWidth = 5;
+        ctx.fillStyle = '#e8e8e8';
+        shapes.circle(ctx, { radius: 15 });
+    });
+}
 
 /**
  * @returns {HTMLCanvasElement} The canvas in page
@@ -29,6 +51,9 @@ function startGame() {
     var context = canvasElement.getContext('2d');
     var engine = createEngine(context);
     engine.start();
+    engine.pushDrawable(new Clear());
+    engine.pushDrawable(cirleDrawable());
+    engine.pushDrawable(new FpsMeter(new Point(20, 20)));
 }
 
 export { startGame };
