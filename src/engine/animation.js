701c27cc4e6d78454cdc30487592ee246ec13980
Mihail Gospodinov <bladespinner@gmail.com>
2017-12-05 01:39:21 +0200

    Implement base animation

--- src/engine/animation.js
+++ src/engine/animation.js
@@ -2,6 +2,7 @@
 
 
 import { Drawable } from './drawable';
+import { linear } from '../math/easing_functions';
 
 /**
  * Drawable class
@@ -12,15 +13,29 @@ class Animation extends Drawable {
      * @param {Point} position The location at which the drawable should.
      * @param {function(Frame, Frame, Drawable):void} render Drawable rendering
      * @param {Number} length Time in miliseconds the animation lasts
-     * @param {Boolean} loop Is the animation looping
+     * @param {{loop: Boolean, alternate: Boolean, easing: function(Number):Number}} options Animation options
      * function.
      */
-    constructor(position, render, length, loop = false) {
+    constructor(position, render, length, options = {
+        loop: false,
+        alternate: true,
+        easing: linear
+    }) {
         super(position, render);
         this._length = length;
-        this._loop = loop;
+        this._options = options;
+        this._offset = 0;
     }
 
+    /**
+     * Calculate the current value of a variable by its start and end
+     * @param {Number} start Starting value of the animated variable
+     * @param {Number} end Ending value of the animated variable
+     * @returns {Number} Current value of the animated variable
+     */
+    animate(start, end) {
+        return start + (end - start) * this._offset;
+    }
 
     /**
      * Draw the drawable
@@ -29,12 +44,27 @@ class Animation extends Drawable {
      * @returns {void}
      */
     draw(prevFrame, curFrame) {
-        if (!this._start) {
-            this._start = curFrame.timestamp;
-            this._progress = 0;
+        var runTime = curFrame.timestamp - this.startingFrame.timestamp;
+        this._offset = runTime / this._length;
+
+        if (this._options.loop) {
+            var loopNumber = Math.floor(this._offset);
+            this._offset -= loopNumber;
+            if (this._options.alternate && loopNumber % 2 === 1) {
+                this._offset = 1 - this._offset;
+            }
+        } else if (this._offset >= 1) {
+            this.finish();
+            if (this._offset == 1) {
+                super.draw(prevFrame, curFrame);
+            }
+            return;
         }
+
+        this._offset = this._options.easing(this._offset);
+
         super.draw(prevFrame, curFrame);
     }
 }
 
-export { Drawable };
+export { Animation };
