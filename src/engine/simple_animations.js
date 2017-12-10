12dc49a625d0488ea2672e22a8c409b6db83169e
Mihail Gospodinov <bladespinner@gmail.com>
2017-12-10 20:27:31 +0200

    Create animations following a path

--- src/engine/simple_animations.js
+++ src/engine/simple_animations.js
@@ -2,6 +2,8 @@
 
 import { Animation } from './animation';
 import { Point } from '../math/point';
+import { Frame } from '../engine/frame';
+import { circularPath, arcPath, bezierCurvePath } from '../math/paths';
 
 /**
  * @typedef {function(Frame,Frame,Drawable):void} Animator
@@ -10,6 +12,8 @@ import { Point } from '../math/point';
  * @typedef {{loop: Boolean, alternate: Boolean, easing: EasingFn}} AnimOptions
  * @typedef {{from: Object, to: Object}} Transition
  * @typedef {function(Drawable,Number,Transition,AnimOptions):Animation} AnimFactory
+ *
+ * @typedef {function(Number):Point} PathingFunction
  */
 
 /**
@@ -45,16 +49,96 @@ function createAnimationFactory(animate) {
  * @return {void}
  */
 function move(prevFrame, curFrame, transition, drawable) {
+    var ctx = curFrame.context;
     var fromPt = transition.from;
     var toPt = transition.to;
 
-    drawable.position = new Point(
+    var pos = new Point(
         this.animate(fromPt.x, toPt.x),
         this.animate(fromPt.y, toPt.y)
     );
+
+    ctx.save();
+    ctx.translate(pos.x, pos.y);
     drawable.draw(prevFrame, curFrame);
+    ctx.restore();
+}
+
+/**
+ * @param {Frame} prevFrame a
+ * @param {Frame} curFrame a
+ * @param {PathingFunction} path a
+ * @param {Drawable} drawable a
+ * @this {Animation}
+ * @return {void}
+ */
+function followPath(prevFrame, curFrame, path, drawable) {
+    var progress = this.animate(0, 1);
+    var pos = path(progress);
+    var ctx = curFrame.context;
+
+    ctx.save();
+    ctx.translate(pos.x, pos.y);
+    drawable.draw(prevFrame, curFrame);
+    ctx.restore();
 }
 
 var moveAnimation = createAnimationFactory(move);
+var pathAnimation = createAnimationFactory(followPath);
 
-export { moveAnimation };
+/**
+ *
+ * @param {*} drawable a
+ * @param {*} length a
+ * @param {*} pathOptions a
+ * @param {*} options a
+ * @return {*} a
+ */
+function arcAnimation(drawable, length, pathOptions, options) {
+    var path = arcPath(
+        pathOptions.radius,
+        pathOptions.from.angle,
+        pathOptions.to.angle
+    );
+
+    return pathAnimation(drawable, length, path, options);
+}
+
+/**
+ *
+ * @param {*} drawable a
+ * @param {*} length a
+ * @param {*} pathOptions a
+ * @param {*} options a
+ * @returns {*} a
+ */
+function circleAnimation(drawable, length, pathOptions, options) {
+    var path = circularPath(
+        pathOptions.radius,
+        pathOptions.initial,
+        pathOptions.clockwise
+    );
+
+    return pathAnimation(drawable, length, path, options);
+}
+
+/**
+ *
+ * @param {*} drawable a
+ * @param {*} length a
+ * @param {*} pathOptions a
+ * @param {*} options a
+ * @returns {*} a
+ */
+function bezierAnimation(drawable, length, pathOptions, options) {
+    var path = bezierCurvePath(pathOptions.points);
+    return pathAnimation(drawable, length, path, options);
+}
+
+export {
+    moveAnimation,
+    pathAnimation,
+    circleAnimation,
+    arcAnimation,
+    bezierAnimation
+};
